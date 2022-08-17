dec622fbb5a022f2c7eed62419cc652443cc9372
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2022-08-18 00:01:45 +0100

    Prototyped adding new data to indexed database

--- indexer.py
+++ indexer.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy as np
 from structures import mp
 import logging
-
+from database import DataBase
 
 def main():
     index()
 
 
-def index():
+def get_items_from_input():
     all_items = []
     for year in range(2019, 2200):
         try:
@@ -24,12 +24,38 @@ def index():
             logging.info(f'found years up to {year - 1}')
             break
 
-    items_table = pd.DataFrame(data=all_items, columns= list(mp.keys()))
+    items_table = pd.DataFrame(data=all_items, columns=list(mp.keys()))
+    items_table['Value'] = items_table['Value'].astype(np.float64)
+    items_table['Balance'] = items_table['Balance'].astype(np.float64)
+    items_table = items_table.replace(r'^\s*$', np.nan, regex=True)
     items_table.index.name = 'key'
-    items_table.to_csv('database/all.csv')
+    return items_table
+
+
+def find_new(df1, df2):
+    return df1[~df1.apply(tuple,1).isin(df2.apply(tuple,1))]
+
+
+def index():
+    incoming_table = get_items_from_input()
+    print(incoming_table.info())
+    print(incoming_table.head())
+    current_table = DataBase.get_database()
+    print(current_table.info())
+    print(current_table.head())
+
+    print('\n##################\n')
+
+    new = find_new(incoming_table, current_table)
+
+    print(new)
+
+    print(pd.concat([current_table, new], ignore_index=True))
 
 
 if __name__ == '__main__':
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 300)
     logging.basicConfig(filename='logs/indexer.log', filemode='w',
                         format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     main()
--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
+df1=pd.DataFrame({'A':[1,2,3,3],'B':[2,3,4,4]})
+df2=pd.DataFrame({'A':[1],'B':[2]})
+df1.index.name = 'key'
+df2.index.name = 'key'
 
-dt = pd.read_csv('database/all.csv', index_col='key')
-
-dt.to_csv('database/all.csv')
+print(pd.concat([df1, df2]).drop_duplicates(keep=False))
 
