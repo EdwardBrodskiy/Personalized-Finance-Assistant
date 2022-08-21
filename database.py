7767cda175f07f986019328d50cefe9d42b5954c
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2022-08-21 21:22:37 +0100

    Joined all existing labels from sheets

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from structures import database_types
 class DataBase:
     @staticmethod
     def get_database():
-        database = pd.read_csv('database/all.csv', index_col='key')
+        database = pd.read_csv('database/protected/all.csv', index_col='key')
         return database.astype(database_types)
 
     @staticmethod
@@ -14,12 +14,12 @@ class DataBase:
         if len(new_items):
             # Load and back-up
             database = DataBase.get_database()
-            database.to_csv('database/previous_all.csv')
+            database.to_csv('database/protected/previous_all.csv')
 
             # Merge and Save
             new_database = pd.concat([database, new_items], ignore_index=True)
             new_database.index.name = 'key'
-            new_database.to_csv('database/all.csv')
+            new_database.to_csv('database/protected/all.csv')
 
             logging.info(f'Saved database with {len(new_items)} new rows')
         else:
---
+++ sheets_importer.py
@@ -0,0 +1,102 @@
+import pandas as pd
+import numpy as np
+from database import DataBase
+from other import sep
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 300)
+
+
+def main():
+    existence = pd.read_csv('sheets_ingest/Spending - Tax.csv', usecols=['Date', 'Who', 'Amount'])
+    existence['Sub Account'] = pd.Series('existence', index=range(len(existence)))
+    existence = existence.astype({'Date': np.datetime64, 'Who': 'category', 'Amount': np.float64, 'Sub Account': 'category'})
+
+    life = pd.read_csv('sheets_ingest/Spending - Personal.csv', usecols=['Date', 'Who', 'What', 'Description', 'Amount', 'Running Total'])
+    life['Sub Account'] = pd.Series('life', index=range(len(life)))
+    life = life.astype(
+        {'Date': np.datetime64, 'Who': 'category', 'What': 'category', 'Description': 'string', 'Amount': np.float64,
+         'Running Total': np.float64,
+         'Sub Account': 'category'})
+
+    ingest = pd.concat([existence, life], sort=False, ignore_index=True)
+    ingest['ref2'] = ingest.index
+
+    print(f'Labeled entries: {len(ingest)}')
+
+    database = DataBase.get_database()
+
+    database['ref'] = database.index
+
+    # merge exact
+
+    joined = database.merge(ingest, left_on=['Date', 'Value'], right_on=['Date', 'Amount'], how='outer', indicator=True)
+
+    # save the successful ones
+
+    good = joined[joined['_merge'] == 'both']
+    good = good.rename(columns={'Description_y': 'Description'})
+    good = good.sort_values('Date')
+
+    # remove cross over duplicates
+
+    # get the list of all duplicate entries
+    dups = good.duplicated(subset=['ref2'])
+    dups = good[good['ref2'].isin(good[dups]['ref2'])]
+    dups = dups.sort_values('ref2')[['ref', 'ref2']]
+
+
+    preserved_left, preserved_right = set() , set()
+    to_remove = []
+    for index, row in dups.iterrows():
+        ref = row['ref']
+        ref2 = row['ref2']
+        if ref in preserved_left or ref2 in preserved_right:
+            to_remove.append(index)
+        else:
+            preserved_left.add(ref)
+            preserved_right.add(ref2)
+
+    good = good.drop(index=to_remove)
+
+    labeled_cols = ['Date', 'Who', 'ref', 'What', 'Description', 'Amount', 'Sub Account', 'ref2']
+    good = good[labeled_cols]
+
+    # extract left and right orphans
+    bad = joined[joined['_merge'] == 'right_only'].dropna(axis=1, how='all').drop('_merge', axis=1)
+
+    non_labeled = joined[joined['_merge'] == 'left_only'].dropna(axis=1, how='all').drop('_merge', axis=1)
+
+    # Try loose join on amount
+
+    bad['_merge_amount'] = np.round(bad['Amount'] * 10).astype(int)
+    non_labeled['_merge_value'] = np.round(non_labeled['Value'] * 10).astype(int)
+    loose_joined = non_labeled.merge(bad, left_on=['Date', '_merge_value'], right_on=['Date', '_merge_amount'], how='outer', indicator=True)
+
+    # clean loose join
+
+    loose_good = loose_joined[loose_joined['_merge'] == 'both']
+    loose_good = loose_good.rename(columns={'Description_y': 'Description'})
+    loose_good = loose_good[labeled_cols]
+    loose_good = loose_good.rename(columns={'Value': 'Amount'})
+
+    # join with the final and save
+
+    final = pd.concat([good, loose_good], ignore_index=True)
+    print(f'Final entries: {len(final)}')
+    dups = final[final.duplicated(subset=['ref2'])]
+    print(dups)
+    print(database[database['Date'].isin(dups['Date']) & database['Description'].str.contains('TRAVEL')])
+    final.to_csv('database/merged.csv')
+
+    # extract left and right orphans again
+
+    bad = loose_joined[loose_joined['_merge'] == 'right_only'].dropna(axis=1, how='all').drop('_merge', axis=1)
+
+    print(f'Bad entries: {len(bad)}')
+
+    non_labeled = loose_joined[loose_joined['_merge'] == 'left_only'].dropna(axis=1, how='all').drop('_merge', axis=1)
+
+
+if __name__ == '__main__':
+    main()
