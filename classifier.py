d0b6c321c511b7b4ed71b231813b7b1bc7dc9475
EdwardBrodskiy <49645295+EdwardBrodskiy@users.noreply.github.com>
2022-08-23 22:24:54 +0100

    Nearly finished user input section

--- classifier.py
+++ classifier.py
@@ -1,7 +1,8 @@
-from structures import merged_types
+from structures import merged_types, database_types
 import json
 import pandas as pd
 from database import DataBase
+from itertools import zip_longest
 
 
 class Classifier:
@@ -53,8 +54,8 @@ class Classifier:
         with open('classification_data/life.json') as file:
             life_keys = json.load(file)
 
-        labeled_data = pd.DataFrame(merged_types, index=[])
-        labeled_data = labeled_data.astype(merged_types)
+        part_labeled = pd.DataFrame(merged_types, index=[])
+        part_labeled = part_labeled.astype(merged_types)
 
         for who, keys in life_keys.items():
             if not keys:
@@ -71,12 +72,101 @@ class Classifier:
 
                 })
                 labeled = labeled.astype(merged_types)
-                labeled_data = pd.concat([labeled_data, labeled], ignore_index=True)
+                part_labeled = pd.concat([part_labeled, labeled], ignore_index=True)
 
-        data = data.drop(index=data[data['ref'].isin(labeled_data['ref'])].index)
-        print(data.head(60))
+        labeled_data, data = self._manual_entry(data, part_labeled)
+
+        data = data.drop(index=data[data['ref'].isin(part_labeled['ref'])].index)
+
+        return part_labeled, data
+
+    def _manual_entry(self, data, part_labeled):
+        db = DataBase()
+        merged = db.get_merged()
+
+        labeled_data = pd.DataFrame(merged_types, index=[])
+        labeled_data = labeled_data.astype(merged_types)
+
+        data = data.sort_values('Date')
+
+        # get categories used and map them to a number (index)
+        mappings = {key: list(merged[key].value_counts().keys()) for key in ('Who', 'What', 'Sub Account')}
+
+        for index, row in data.iterrows():
+            part_labeled_row = part_labeled[part_labeled['ref'] == row['ref']]
+
+            # work out what information we need from user
+            if not len(part_labeled_row):
+                part_labeled_row = None
+                expected_fields = ['Who', 'What', 'Description', 'Sub Account', 'Amount']
+                continue
+            else:
+                expected_fields = ['What', 'Description', 'Sub Account', 'Amount']
+                part_labeled_row = part_labeled_row.reset_index().iloc[0]  # extract the single row
+
+            # User information display
+            if part_labeled_row is not None:
+                part_labeled_row = part_labeled_row.to_frame().T.drop('index', axis=1)
+
+                print(f'Part Labeled row:\n{part_labeled_row}')
+
+            for key, mapping in mappings.items():
+                if key in expected_fields:
+
+                    text_pairs = [f'{value}  {i}' for i, value in enumerate(mapping)]
+                    max_length = max(map(len, text_pairs))
+                    text_pairs = list(map(lambda x: x.ljust(max_length + 2), text_pairs))
+                    chars_wide = 140
+                    display_columns = chars_wide // max_length
+                    display_rows = len(text_pairs) // display_columns + 1
+
+                    print(f'\nCategories for {key}:\n')
+                    for i in range(display_rows):
+                        print('|'.join(text_pairs[i * display_columns: (i + 1) * display_columns]))
+
+            print(f'\nIncoming data to be labeled:\n{row.to_frame().T}')
+            user_input = input(f'Enter {", ".join(expected_fields)}\n>>>')
+            if user_input == 'finish':
+                break
+            elif user_input in ['exit', 'cancel']:
+                exit()
+            elif user_input in ['s', 'skip', '']:
+                continue
+            else:
+                user_input = list(map(lambda x: x.strip(), user_input.split(',')))
+                entered_data = {key: None if value == '' else value for key, value in zip_longest(expected_fields, user_input)}
+
+                # convert number shortcut to full category name
+                for key, mapping in mappings.items():
+                    if key in expected_fields:
+                        entered_data[key] = self._process_category(mapping, entered_data[key], key)
+
+                        if entered_data[key] not in mapping:
+                            mappings[key].append(entered_data[key])
+
+                print(entered_data)
+                if part_labeled_row is not None:
+                    for key, value in entered_data.items():
+                        if value is None:
+                            continue
+                        if key == 'Amount':
+                            part_labeled_row.at[0,key] = float(value)
+                        else:
+                            part_labeled_row.at[0,key] = value
+                    print(part_labeled_row)
 
         return labeled_data, data
+
+    @staticmethod
+    def _process_category(mapping, entry, key):
+        while True:
+            try:
+                return mapping[int(entry)]
+            except ValueError:
+                return entry
+            except IndexError:
+                entry = input(f'Category does not exist in {key} please re enter: ')
+
 
 
 def main():
@@ -84,7 +174,6 @@ def main():
     db = DataBase()
     data = db.get_database()
     merged = db.get_merged()
-    print(merged[merged['Sub Account'] == 'life']['Who'].value_counts())
     joined = data.merge(merged, how='outer', left_on=['ref'], right_on=['ref'], indicator=True)
 
     good = joined[joined['_merge'] == 'both'].drop('_merge', axis=1)
@@ -92,11 +181,10 @@ def main():
     un_labeled = joined[joined['_merge'] == 'left_only'].dropna(axis=1, how='all').drop('_merge', axis=1)
 
     un_labeled = un_labeled.rename(columns={'Description_x': 'Description'})
-    print(un_labeled.info())
+
     existence_labeled, un_labeled = cl.classify_existence_certain(un_labeled)
     life_labeled, un_labeled = cl.classify_life(un_labeled)
     db.add_to_merged(existence_labeled)
-
 
 
 if __name__ == '__main__':
